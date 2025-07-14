7a22add7c2db500c0e011977e7ea4210a88aa25f
te-ce <9517685+te-ce@users.noreply.github.com>
2025-07-14 22:24:01 +0200

    vue: tailwind and init cleanup

---
+++ apps/frontend/vue3-composition/src/components/NoteHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <header>
+    <h1>Vue3 Composition Notes App</h1>
+  </header>
+</template>
--- apps/frontend/vue3-composition/src/views/HomeView.vue
+++ apps/frontend/vue3-composition/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Header from '../components/NoteHeader.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="flex flex-col items-center justify-center h-screen bg-zinc-900 text-white">
+    <Header />
   </main>
 </template>
